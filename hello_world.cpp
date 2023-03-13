74fe9c9866c0e7b7a40e61b1f533b461bdc891f7
psychotlc <narench@inbox.ru>
2023-03-13 13:39:11 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main(){
     std::string name;
-    std::cout << "Enter name: ";
+    std::cout << "Enter name: "; //Enter name
     std::cin >> name; 
-    std::cout<<"Hello world " << name << std::endl;
+    std::cout<<"Hello world " << name << std::endl; //Out hello
     return 0;
 } 
