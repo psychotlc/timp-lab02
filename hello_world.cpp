bc010653eb398a27863e1def26a38e65b6eee00b
psychotlc <narench@inbox.ru>
2023-03-13 14:15:24 +0300

    Russian comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main(){
     std::string name;
-    std::cout << "Enter name: "; //Enter name
+    std::cout << "Enter name: "; //Введите имя
     std::cin >> name; 
-    std::cout<<"Hello world " << name << std::endl; //Out hello
+    std::cout<<"Hello world " << name << std::endl; //Выводит привет
     return 0;
 } 
